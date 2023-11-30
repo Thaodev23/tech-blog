8c0e458069e1f1d302c42df5eaeeb2feb532332c
Joey Thao <thaojoey5@gmail.com>
2023-11-29 20:55:07 -0600

    pushing final touches of HW14

---
+++ seeds/seed.js
@@ -0,0 +1,32 @@
+const sequelize = require('../config/connection');
+const { User, Post, Comment } = require('../models');
+
+const userData = require('./userData.json');
+const postData = require('./postData.json');
+const commentData = require('./commentData.json');
+
+const seedDatabase = async () => {
+  await sequelize.sync({ force: true });
+
+  for (const user of userData) {
+    await User.create({
+      ...user
+    });
+  }
+
+  for (const post of postData) {
+    await Post.create({
+      ...post
+    });
+  }
+
+  for (const comment of commentData) {
+    await Comment.create({
+      ...comment
+    })
+  }
+
+  process.exit(0);
+};
+
+seedDatabase();
